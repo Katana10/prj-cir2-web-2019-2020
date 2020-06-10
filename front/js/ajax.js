36ad51d298bff76fa27225a373fba5b749facb85
MatheoBailly <66616107+MatheoBailly@users.noreply.github.com>
2020-06-10 11:04:59 +0200

    Add files via upload

---
+++ front/js/ajax.js
@@ -0,0 +1,37 @@
+function ajaxRequest(type, url, callback, data = null) {
+    let xhr = new XMLHttpRequest();
+    if (type == 'GET' && data != null)
+        url += '?' + data;
+    xhr.open(type, url);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    console.log(url);
+
+    xhr.onload = () => {
+        switch(xhr.status) {
+            case 200:
+            case 201:
+                if(xhr.responseText.length != 0) callback(JSON.parse(xhr.responseText));
+                break;
+            default: 
+                httpErrors(xhr.status);
+        }
+    };
+    xhr.send(data);
+}
+
+
+function httpErrors(errorCode){
+    let messages ={
+        400: 'Requête incorrecte',
+        401: 'Authentifiez-vous',
+        403: 'Accès Refusé',
+        404: 'Page non trouvé',
+        500: 'Erreur interne du serveur',
+        503: 'Service indisponible'
+    };
+    $('#errors').html('<i class="fa fa-exclamation-circle"></i><strong>' +messages[errorCode]+'</strong>');
+    $('#errors').show();
+}
+
+
+
