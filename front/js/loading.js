5c9a26f272b4a7f2079d2992e9aadf46d62eb510
MatheoBailly <66616107+MatheoBailly@users.noreply.github.com>
2020-06-12 16:42:18 +0200

    Add files via upload

--- front/js/loading.js
+++ front/js/loading.js
@@ -4,7 +4,7 @@
 function loadCyclists(cyclists){
     var club = "";
     cyclists.forEach(function(element){
-		club += fillcyclist(element['mail'], element['nom'],element['prenom'],
+		club += fillCyclists(element['mail'], element['nom'],element['prenom'],
 		elment['num_licence'],element['date_naissance'],element['valide'],
 		element['club'],element['code_insee'],element['categorie'],
 		element['categorie_categorie_valeur']);
@@ -15,73 +15,57 @@ function loadCyclists(cyclists){
 }
 
 //Renvoie une div pour chaque cyclist
-function fillcyclist(mail, nom, prenom, num_licence, date_naissance, valide, club, code_insee, categorie, categorie_categorie_valeur){
-	var cyclist_card = "<div class='col-xs-2 col-md-2'><a href='#'><p>mail :"+mail+"</p>"+
+function fillCyclists(mail, nom, prenom, num_licence, date_naissance, valide, club, code_insee, categorie, categorie_categorie_valeur){
+	var cyclist_card = "<div class='col-xs-2 col-md-2'><a href='#'><div value="+mail+"><p>mail :"+mail+"</p>"+
 	"<p>nom :"+nom+"</p>"+"<p>prenom :"+prenom+"</p>"+"<p>numéro de licence :"+num_licence+"</p>"+
 	"<p>date de naissance :"+date_naissance+"</p>"+"<p>inscription valide :"+valide+"</p>"+
 	"<p>Club :"+club+"</p>"+"<p>Code INSEE :"+code_insee+"</p>"+"<p>categorie :"+categorie+
-	" ("+categorie_categorie_valeur+")</p>"+"</a></div>";
+	" ("+categorie_categorie_valeur+")</p>"+"</div></a></div>";
     
     return cyclist_card;
 }
 
-/*//chargement de la photo dans photo et organisation en div grace à fillMaxi
-function LoadCyclist(cyclist){
-    ajaxRequest('GET', 'php/request.php/comments/?id='+photo['id'], loadComments);
-    var emplacement = "";
-    emplacement += fillMaxi(photo['id'], photo['src'], photo['title']);
+function loadCyclist(cyclist){
+    var card = "";
+	card = fillCyclists(element['mail'], element['nom'],element['prenom'],
+	elment['num_licence'],element['date_naissance'],element['valide'],
+	element['club'],element['code_insee'],element['categorie'],
+	element['categorie_categorie_valeur']);
     
-    $('#photo').attr('photoid', photo['id']);
-    $('#photo').html(emplacement);
-
+    $('#cyclist').html(card);
     
 }
 
-//Renvoie une div pour la photo en grand
-function fillMaxi(id, path, title) {
-    var miniature = "<div class='col-xs-12 col-md-12'><a href='#'><h4>"+title+"</h4><img value="+id+" src = "+path+" class='img-thumbnail' ></a></div>";
+//Renvoie une div pour le cycliste sélectionné avec des boutons pour modifier 
+function fillCyclist(mail, nom, prenom, num_licence, date_naissance, valide, club, code_insee, categorie, categorie_categorie_valeur){
+    var cyclist_card = "<div class='col-xs-2 col-md-2'><a href='#'><p>mail :"+mail+"</p>"+
+	"<p>nom :"+nom+"</p>"+"<p>prenom :"+prenom+"</p>"+"<p>numéro de licence :"+num_licence+"</p>"+
+	"<p>date de naissance :"+date_naissance+"</p>"+"<p>inscription valide :"+valide+"</p>"+
+	"<p>Club :"+club+"</p>"+"<p>Code INSEE :"+code_insee+"</p>"+"<p>categorie :"+categorie+
+	" ("+categorie_categorie_valeur+")</p>"+"<button type='button' class='btn btn-light float-right mod'"+
+	"value= " + mail + "><i class='fa fa-edit'></i></button>"+"</a></div>";
     
-    return miniature;
-}*/
-
-//Renvoie une div pour chaque commentaire avec des boutons pour supprimer et modifier
-//A NOTER j'ai essayé de l'appeler displayComment au début mais ça ne marchait pas 
-function fillComment(user, texte, id){
-    var comment = "<div class='panel-body'>" +
-				user + ": " + texte +
-				"<button type='button' class='btn btn-light float-right mod'"+ 
-				"value= " + id + "><i class='fa fa-edit'></i></button>"+
-				"<button type='button' class='btn btn-light float-right del'"+
-				"value= " + id + "><i class='fa fa-trash'></i></button></div><br>";
-	return comment;
+    return cyclist_card;
+				
 }
 
-//charge les commmentaires de la photo choisie
-function loadComments(elements){
-    document.getElementById("comments").style.display = 'block';
-	var emplacement = '<div class="panel panel-default"><br>';
-	elements.forEach( function(comment) {
-		emplacement += fillComment(comment['userLogin'], comment['comment'], comment['id']);
-	});
-	emplacement += "</div>";
-	$('#comment-list').html(emplacement);
-}
-
-
-
-
-ajaxRequest('GET','php/request.php/photos/', loadPhotos);
+//Method Put
+$('#cyclist-card').off('click').on('click', '.mod', () =>{
+	ajaxRequest('PUT', 'http://prj-cir2-web-api.monposte/request.php/cyclistes/' + $(event.target).closest('.mod').attr('value'), () =>{
+		ajaxRequest('GET', 'http://prj-cir2-web-api.monposte/request.php/cyclistes/', loadCyclists);
+	}, 'modificate=' + prompt('Modification du champ :'));
+});
 
 //Attente de l'évènement pour cliquer sur une image
-$('#thumbnail').on('click', 'img', function() {
+$('#cyclistes').on('click', 'img', function() {
     var id = $(event.target).attr('value');
     console.log(id);
     
-    ajaxRequest('GET','php/request.php/photos/'+id, LoadPhoto);
+    ajaxRequest('GET','http://prj-cir2-web-api.monposte/request.php/cyclistes/'+id, loadCyclist);
    
-});
+});*/
 
-
+ajaxRequest('GET', 'http://prj-cir2-web-api.monposte/request.php/cycliste/', loadCyclists);
 
 //Attente de l'évènement pour modifier un commentaire
 $('#comment-list').on('click', '.mod', () => {
@@ -91,19 +75,3 @@ $('#comment-list').on('click', '.mod', () => {
 		}, 'comment=' + prompt('Nouveau commentaire: '));
 });
 
-//Attente de l'évènement pour supprimer un commentaire
-$('#comment-list').on('click', '.del', () => {
-	ajaxRequest('DELETE', 'php/request.php/comments/' + 
-		$(event.target).closest('.del').attr('value'), () => {
-			ajaxRequest('GET', 'php/request.php/comments/?id='+$('#photo').attr('photoid'), loadComments);
-		});
-});
-
-//Attente de l'évènement pour poster un commentaire
-$('#comments-add').on('submit', (event) => {
-	event.preventDefault();
-	ajaxRequest('POST', 'php/request.php/comments/', () => {
-		ajaxRequest('GET', 'php/request.php/comments/?id='+ $('#photo').attr('photoid'), loadComments);
-	}, 'id=' + $('#photo').attr('photoid') + '&comment=' + $('#textComment').val());
-	$('#textComment').val('');
-});
